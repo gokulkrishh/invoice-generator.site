2e9a47885a974b0e80301eb8836f814050abdf9e
gokulkrishh <2944237+gokulkrishh@users.noreply.github.com>
2025-10-12 19:04:43 +0530

    Enhance invoice generation form with validation using Zod and React Hook Form; update global styles for improved theming and add new dependencies in package.json.

---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="flex">
+      <h1 className="text-2xl font-bold mx-auto max-w-6xl">
+        Invoice Generator
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
